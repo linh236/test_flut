66ce04e290c7ded177b9ff41852a3d7bad1f581b
Minh Nguyen <3lang@gmail.com>
2021-05-15 00:20:50 +0700

    change login and main

--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Card(
+        child: Column(children: [Text('Nguyên đẹp trai, cao raossssssssss')]),
+      ),
+    );
   }
 }
